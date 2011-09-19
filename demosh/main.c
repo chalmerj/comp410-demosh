a1df257d71a53de3ca0a15289e24d158e2b860f8
Jeremy Chalmer <chalmer.j@gmail.com>
2011-09-19 16:58:15 -0500

    Initial Commit

---
+++ demosh/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  demosh
+//
+//  Created by Jeremy Chalmer on 9/19/11.
+//  Copyright 2011 Loyola University. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
